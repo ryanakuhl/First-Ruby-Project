1d04ef66d53f07f8fbd3e356f85afa97b976f775
Ryan Kuhl <ryanakuhl@gmail.com>
2015-01-31 10:43:35 -0500

    simplify

--- spec/features/todo_lists/create_spec.rb
+++ spec/features/todo_lists/create_spec.rb
@@ -8,8 +8,8 @@ describe "Create todo lists" do
 		click_link "New Todo list"	
 		expect(page).to have_content("New todo_list")
 	
-		fill_in "Title", with: options:[:title]
-		fill_in "Description", with: options:[:description]
+		fill_in "Title", with: options[:title]
+		fill_in "Description", with: options[:description]
 		click_button "Create Todo list"
 	
 	end
@@ -34,13 +34,7 @@ end
 it "Displays an error when the todo list has a title less than 3 characters" do
 	expect(TodoList.count).to eq(0)
 
-	visit "/todo_lists"
-	click_link "New Todo list"	
-	expect(page).to have_content("New todo_list")
-
-fill_in "Title", with: "Hi"
-fill_in "Description", with: "This is what I'm going today."
-click_button "Create Todo list"
+create_todo_list title: "Hi"
 
 expect(page).to have_content("error")
 expect(TodoList.count).to eq(0)
@@ -52,13 +46,7 @@ end
 it "Displays an error when the todo list has no description" do
 	expect(TodoList.count).to eq(0)
 
-	visit "/todo_lists"
-	click_link "New Todo list"	
-	expect(page).to have_content("New todo_list")
-
-fill_in "Title", with: "Grocery list"
-fill_in "Description", with: ""
-click_button "Create Todo list"
+create_todo_list title: "Grocery list", description: ""
 
 expect(page).to have_content("error")
 expect(TodoList.count).to eq(0)
@@ -67,16 +55,10 @@ visit "/todo_lists"
 expect(page).to_not have_content("Grocery list")
 end
 
-it "Displays an error when the todo list a description less than 5 characters" do
+it "Displays an error when the todo list has a description less than 5 characters" do
 	expect(TodoList.count).to eq(0)
 
-	visit "/todo_lists"
-	click_link "New Todo list"	
-	expect(page).to have_content("New todo_list")
-
-fill_in "Title", with: "Grocery list"
-fill_in "Description", with: "Food"
-click_button "Create Todo list"
+create_todo_list title: "Grocery list", description: "Food"
 
 expect(page).to have_content("error")
 expect(TodoList.count).to eq(0)
